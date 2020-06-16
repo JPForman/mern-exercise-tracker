efc978f264f000bb23fa45af98bc28e3bbc41e07
Joshua Forman <cyborgforman@gmail.com>
2020-06-15 19:17:48 -0700

    connect mongo database

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const mongoose = require ('mongoose');
+const mongoose = require('mongoose');
 
 require('dotenv').config();
 
@@ -19,5 +19,5 @@ connection.once('open', () => {
 })
 
 app.listen(port, () => {
-  console.log('Server is running on port:', port);
+    console.log(`Server is running on port: ${port}`);
 });
