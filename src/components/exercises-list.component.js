0011ab3e07237d2261814264e966892fdae09d29
Joshua Forman <cyborgforman@gmail.com>
2020-06-17 10:24:08 -0700

    add crate exercise component

---
+++ src/components/exercises-list.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ExercisesList extends Component {
+  render() {
+    return (
+      <div>
+        <p>Hello from exercises list component</p>
+      </div>
+    )
+  }
+}
