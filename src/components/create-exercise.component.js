0c581d2d71e21ed2a8e57d3fa282b0ed723e7297
Joshua Forman <cyborgforman@gmail.com>
2020-06-17 17:11:14 -0700

    populate exercise dropdown with username list from database

--- src/components/create-exercise.component.js
+++ src/components/create-exercise.component.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import DatePicker from 'react-datepicker';
+import axios from 'axios';
 import 'react-datepicker/dist/react-datepicker.css';
 
 export default class CreateExercise extends Component {
@@ -16,10 +17,15 @@ export default class CreateExercise extends Component {
   }
 
   componentDidMount() {
-    this.setState({
-      users: ['test user'],
-      username: 'test user'
-    })
+    axios.get('http://localhost:5000/users/')
+      .then(response => {
+        if (response.data.length > 0) {
+          this.setState({
+            users: response.data.map(user => user.username),
+            username: response.data[0].username
+          })
+        }
+      })
   }
   
   onChangeUsername = (e) => {
@@ -56,6 +62,10 @@ export default class CreateExercise extends Component {
       date: this.state.date
     }
     console.log(exercise);
+
+    axios.post('http://localhost:5000/exercise/add', exercise)
+      .then(res => console.log(res.data));
+  
     
     window.location= '/';
     
